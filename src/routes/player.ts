f2dec86547593d746f3bba97c93a4ebe9e905028
marta.pancaldi <marta.pancaldi@resourcify.de>
2023-05-15 20:47:03 +0200

    Add endpoint to search Players from Region looking for Region

--- src/routes/player.ts
+++ src/routes/player.ts
@@ -1,33 +1,23 @@
-import {Prisma, PrismaClient} from "@prisma/client";
 import express from "express";
 import {PlayerFetchingError} from "../models/PlayerFetchingError";
 import {PlayerFetchResult} from "../models/PlayerFetchResult";
-import {getPlayersFromRegion, getRegionFromName} from "../service/matchingService";
-
-const prisma = new PrismaClient();
+import {
+    getAllPlayers,
+    getPlayer,
+    getPlayersFromRegion, getPlayersFromRegionWithWantedHighPrioRegion, getPlayersFromRegionWithWantedLowPrioRegion,
+    getPlayersWithWantedHighPrioRegion,
+    getPlayersWithWantedLowPrioRegion,
+    getRegionFromName
+} from "../service/matchingService";
+import {fr} from "@faker-js/faker";
+import region from "./region";
 
 const router = express.Router();
 
 router.get('/players', async (req, res) => {
     const {search, orderBy} = req.query
 
-    const or: Prisma.PlayerWhereInput = search
-        ? {
-            OR: [
-                {nickname: {contains: search as string}},
-                {location: {contains: search as string}},
-            ],
-        }
-        : {};
-
-    const players = await prisma.player.findMany({
-        where: {
-            ...or
-        },
-        orderBy: {
-            id: orderBy as Prisma.SortOrder || 'asc'
-        }
-    });
+    const players = await getAllPlayers(search, orderBy);
 
     res.json(players.sort(p => p.id));
 });
@@ -36,11 +26,7 @@ router.get('/players/:id', async (req, res) => {
     try {
         const id = parseInt(req.params.id);
 
-        const player = await prisma.player.findUnique({
-            where: {
-                id: id
-            }
-        });
+        const player = await getPlayer(id);
 
         res.status(404).json(player ? player : {error: `Player with id ${id} not found.`});
 
@@ -51,6 +37,7 @@ router.get('/players/:id', async (req, res) => {
 
 router.get('/players/from/:regionName', async (req, res) => {
     const regionName = req.params.regionName.toLowerCase();
+
     const region = await getRegionFromName(regionName)
 
     if (!region) {
@@ -68,49 +55,50 @@ router.get('/players/from/:regionName', async (req, res) => {
 
 router.get('/players/looking-for/:regionName', async (req, res) => {
     const regionName = req.params.regionName.toLowerCase();
-    const region = await prisma.region.findFirst({
-        where: {
-            name: regionName
-        }
-    })
+
+    const region = await getRegionFromName(regionName);
 
     if (!region) {
         res.status(400).json({error: `Region '${regionName}' could not be found.`})
     } else {
-        const highPrioPlayers = prisma.player.findMany({
-            where: {
-                wantedHighPrio: {
-                    some: {
-                        regionId: {
-                            in: [region.id]
-                        }
-                    }
-                }
-            },
-            orderBy: {
-                id: 'asc'
-            }
-        });
-        const lowPrioPlayers = prisma.player.findMany({
-            where: {
-                wantedLowPrio: {
-                    some: {
-                        regionId: {
-                            in: [region.id]
-                        }
-                    }
-                }
-            },
-            orderBy: {
-                id: 'asc'
-            }
-        });
+        const highPrioPlayers = getPlayersWithWantedHighPrioRegion(region);
+        const lowPrioPlayers = getPlayersWithWantedLowPrioRegion(region);
 
         Promise.all([highPrioPlayers, lowPrioPlayers]).then(([highPrioPlayers, lowPrioPlayers]) => {
-            res.json({
-                highPrioPlayers,
-                lowPrioPlayers
-            } as PlayerFetchResult);
+            res.json(
+                {
+                    highPrioPlayers,
+                    lowPrioPlayers
+                } as PlayerFetchResult
+            );
+        }).catch((err) => {
+            res.status(404).json({error: `Error finding matching players: ${err}`} as PlayerFetchingError);
+        })
+    }
+});
+
+router.get('/players/from/:fromRegion/looking-for/:wantedRegion', async (req, res) => {
+    const fromRegionName = req.params.fromRegion.toLowerCase();
+    const wantedRegionName = req.params.wantedRegion.toLowerCase();
+
+    const fromRegion = await getRegionFromName(fromRegionName);
+    const wantedRegion = await getRegionFromName(wantedRegionName);
+
+    if (!fromRegion || !wantedRegion) {
+        const regionNotFound = !fromRegion ? fromRegion : wantedRegion;
+
+        res.status(400).json({error: `Region '${regionNotFound}' could not be found.`})
+    } else {
+        const highPrioPlayersFromRegion = getPlayersFromRegionWithWantedHighPrioRegion(fromRegion, wantedRegion);
+        const lowPrioPlayersFromRegion = getPlayersFromRegionWithWantedLowPrioRegion(fromRegion, wantedRegion);
+
+        Promise.all([highPrioPlayersFromRegion, lowPrioPlayersFromRegion]).then(([highPrioPlayers, lowPrioPlayers]) => {
+            res.json(
+                {
+                    highPrioPlayers,
+                    lowPrioPlayers
+                } as PlayerFetchResult
+            );
         }).catch((err) => {
             res.status(404).json({error: `Error finding matching players: ${err}`} as PlayerFetchingError);
         })
--- src/service/matchingService.ts
+++ src/service/matchingService.ts
@@ -1,4 +1,4 @@
-import {PrismaClient, Region} from "@prisma/client";
+import {Prisma, PrismaClient, Region} from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -14,6 +14,103 @@ export function getPlayersFromRegion(region: Region) {
     return prisma.player.findMany({
         where: {
             regionId: region.id
+        }
+    });
+}
+
+export function getAllPlayers(search: any, orderBy: any) {
+    const or: Prisma.PlayerWhereInput = search ?
+        {
+            OR: [
+                {nickname: {contains: search as string}},
+                {location: {contains: search as string}},
+            ],
+        } : {};
+
+    return prisma.player.findMany({
+        where: {
+            ...or
+        },
+        orderBy: {
+            id: orderBy as Prisma.SortOrder || 'asc'
+        }
+    });
+}
+
+export function getPlayer(id: number) {
+    return prisma.player.findUnique({
+        where: {
+            id: id
+        }
+    });
+}
+
+export function getPlayersWithWantedHighPrioRegion(region: Region) {
+    return prisma.player.findMany({
+        where: {
+            wantedHighPrio: {
+                some: {
+                    regionId: {
+                        in: [region.id]
+                    }
+                }
+            }
+        },
+        orderBy: {
+            id: 'asc'
+        }
+    });
+}
+
+export function getPlayersWithWantedLowPrioRegion(region: Region) {
+    return prisma.player.findMany({
+        where: {
+            wantedHighPrio: {
+                some: {
+                    regionId: {
+                        in: [region.id]
+                    }
+                }
+            }
+        },
+        orderBy: {
+            id: 'asc'
+        }
+    });
+}
+
+export function getPlayersFromRegionWithWantedHighPrioRegion(from: Region, wanted: Region) {
+    return prisma.player.findMany({
+        where: {
+            wantedHighPrio: {
+                some: {
+                    regionId: {
+                        in: [wanted.id]
+                    }
+                }
+            },
+            regionId: from.id
+        },
+        orderBy: {
+            id: 'asc'
+        }
+    });
+}
+
+export function getPlayersFromRegionWithWantedLowPrioRegion(from: Region, wanted: Region) {
+    return prisma.player.findMany({
+        where: {
+            wantedHighPrio: {
+                some: {
+                    regionId: {
+                        in: [wanted.id]
+                    }
+                }
+            },
+            regionId: from.id
+        },
+        orderBy: {
+            id: 'asc'
         }
     });
 }
